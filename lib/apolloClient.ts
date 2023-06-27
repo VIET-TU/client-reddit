4e5cadd195257a71c647b5ee2c8d774532c202aa
unknown <dovandaihoc@gmail.com>
2023-06-27 11:04:28 +0700

    update evn

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -46,8 +46,9 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
 		})
 	}
 
+	console.log('object :>> ', process.env.URL_SERVER_LOCAL)
 	const httpLink = new HttpLink({
-		uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
+		uri: process.env.URL_SERVER_LOCAL, // Server URL (must be absolute)
 		credentials: 'include', // Additional fetch() options like `credentials` or `headers`
 		fetch: enhancedFetch,
 	})
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+	reactStrictMode: true,
+	env: {
+		URL_SERVER: process.env.URL_SERVER,
+		URL_SERVER_LOCAL: process.env.URL_SERVER_LOCAL,
+	},
+}
 
 module.exports = nextConfig
